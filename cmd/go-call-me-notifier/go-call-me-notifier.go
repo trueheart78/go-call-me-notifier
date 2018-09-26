e5beb04da35e9d03aa89ca8acf5d397d40ff8f7e
Josh Mills <josh@trueheart78.com>
2018-09-26 12:35:22 -0400

    basic redis subscribe support

--- cmd/go-call-me-notifier/go-call-me-notifier.go
+++ cmd/go-call-me-notifier/go-call-me-notifier.go
@@ -6,27 +6,77 @@ import (
 	"runtime"
 	"time"
 
+	"github.com/go-redis/redis"
 	"github.com/trueheart78/go-call-me-notifier/internal/pkg/config"
 	"github.com/trueheart78/go-call-me-notifier/internal/pkg/notifier"
 )
 
-var cfg config.Config
+var cfg *config.Config
 
 func init() {
 	if runtime.GOOS != "darwin" {
 		fmt.Printf("Unsupported operating system: %v\n", runtime.GOOS)
 		os.Exit(1)
 	}
-	cfg, err := config.NewConfig()
+	_, err := config.NewConfig()
 	if err != nil {
 		fmt.Println(err.Error())
 		os.Exit(1)
 	}
-	fmt.Printf("Connecting to %v\n", cfg.RedisURL)
 }
 
 func main() {
-	fmt.Println(runtime.GOOS)
+	monitor()
+}
+
+func monitor() {
+	cfg, _ := config.NewConfig()
+	redisdb := redis.NewClient(&redis.Options{
+		Addr:     cfg.RedisURL,
+		Password: cfg.RedisPassword,
+		DB:       0, // use default DB
+	})
+
+	_, err := redisdb.Ping().Result()
+	if err != nil {
+		hasPass := (cfg.RedisPassword != "")
+		fmt.Printf("Error: Unable to connect to redis [password: %v]\n  url: %v\n", hasPass, cfg.RedisURL)
+		os.Exit(1)
+	}
+
+	fmt.Printf("Connected to redis @ %v\n", cfg.RedisURL)
+
+	pubsub := redisdb.Subscribe(cfg.RedisChannels.Emergency, cfg.RedisChannels.NonEmergent)
+
+	// Wait for confirmation that subscription is created before publishing anything.
+	_, err = pubsub.Receive()
+	if err != nil {
+		panic(err)
+	}
+
+	// Go channel which receives messages.
+	ch := pubsub.Channel()
+
+	// When pubsub is closed channel is closed too.
+	defer pubsub.Close()
+
+	fmt.Printf("Listening on '%v' and '%v'...\n", cfg.RedisChannels.Emergency, cfg.RedisChannels.NonEmergent)
+	// Consume messages.
+	var t time.Time
+	for {
+		msg, ok := <-ch
+		if !ok {
+			break
+		}
+		t = time.Now()
+
+		fmt.Printf("Message received on '%v': %v [%v]\n", msg.Channel, msg.Payload, t.Format("Jan 2, 2006 at 3:04pm (MST)"))
+		if msg.Channel == cfg.RedisChannels.Emergency {
+			emergency()
+		} else if msg.Channel == cfg.RedisChannels.NonEmergent {
+			nonemergent()
+		}
+	}
 }
 
 func emergency() {
